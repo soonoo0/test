8b97beca3b0cf70af29174f7082317400824cdbd
KOITT <KOITT@KOITT-PC03>
2017-12-04 18:45:55 +0900

    프로젝트 초기파일 복사본

---
+++ UML01BoardClone/src/main/java/com/koitt/board/model/Book.java
@@ -0,0 +1,140 @@
+package com.koitt.board.model;
+
+public class Book {
+	
+	private Integer isbn;
+	private String title;
+	private String author;
+	private String publisher;
+	private Integer price;
+	private String bdesc;
+	
+	public Book() {}
+
+	public Book(Integer isbn, String title, String author, String publisher, Integer price, String bdesc) {
+		super();
+		this.isbn = isbn;
+		this.title = title;
+		this.author = author;
+		this.publisher = publisher;
+		this.price = price;
+		this.bdesc = bdesc;
+	}
+
+	public Integer getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(Integer isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public String getBdesc() {
+		return bdesc;
+	}
+
+	public void setBdesc(String bdesc) {
+		this.bdesc = bdesc;
+	}
+	
+	@Override
+	public int hashCode() {
+		int result = this.isbn + this.title.hashCode() + this.author.hashCode() +
+				this.publisher.hashCode() + this.price + this.bdesc.hashCode();
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (bdesc == null) {
+			if (other.bdesc != null)
+				return false;
+		} else if (!bdesc.equals(other.bdesc))
+			return false;
+		if (isbn == null) {
+			if (other.isbn != null)
+				return false;
+		} else if (!isbn.equals(other.isbn))
+			return false;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (!price.equals(other.price))
+			return false;
+		if (publisher == null) {
+			if (other.publisher != null)
+				return false;
+		} else if (!publisher.equals(other.publisher))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Book [isbn=");
+		builder.append(isbn);
+		builder.append(", title=");
+		builder.append(title);
+		builder.append(", author=");
+		builder.append(author);
+		builder.append(", publisher=");
+		builder.append(publisher);
+		builder.append(", price=");
+		builder.append(price);
+		builder.append(", bdesc=");
+		builder.append(bdesc);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
